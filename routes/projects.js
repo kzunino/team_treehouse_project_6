74bf79a7e664553c4530da0d29a9fdca3e335ab4
Kyle <kzunino@gmail.com>
2019-06-21 21:15:32 -0700

    routes work, pug template doesn't

---
+++ routes/projects.js
@@ -0,0 +1,20 @@
+// const express = require('express');
+// const router = express.Router();
+// const {data} = require('../data/data.json'); //{data} referes to data.data
+// const {projects} = data;      //data.projects syntax.
+//
+// router.get('/', (req, res) => {
+//   res.render('project', {
+//     photo: projects[0].id
+//   });
+// });
+//
+// // router.get('/:id', (req, res) => {
+// //   res.render('project', {
+// //
+// //
+// //   });
+// // });
+//
+//
+// module.exports = router;
