3ab1fc776438199bb2411fe425a95db65e119d73
Kyle <kzunino@gmail.com>
2019-06-27 13:50:21 -0700

    working on pug interpolation

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,7 @@ const {data} = require('../data/data.json');
 const {projects} = data;      //data.projects syntax.
 
 router.get('/', (req, res) => {         //renders the main page
-  res.render('index', {projects,
-    id: projects[0].id,
-    title: projects[0].project_name,
-    photo: projects[0].image_urls[0]
-  });
-});
-
-router.get('/project', (req, res) => {
-  res.render('project');
+  res.render('index', {projects});
 });
 
 module.exports = router;
--- routes/projects.js
+++ routes/projects.js
@@ -1,20 +1,22 @@
-// const express = require('express');
-// const router = express.Router();
-// const {data} = require('../data/data.json'); //{data} referes to data.data
-// const {projects} = data;      //data.projects syntax.
-//
-// router.get('/', (req, res) => {
-//   res.render('project', {
-//     photo: projects[0].id
-//   });
+const express = require('express');
+const router = express.Router();
+const {data} = require('../data/data.json'); //{data} referes to data.data
+const {projects} = data;      //data.projects es6 syntax.
+
+// router.get('/:id', (req, res) => {
+//   const {id} = req.params;
+//   console.log(id);
+//   res.redirect(`/${id}`);
 // });
-//
-// // router.get('/:id', (req, res) => {
-// //   res.render('project', {
-// //
-// //
-// //   });
-// // });
-//
-//
-// module.exports = router;
+
+router.get('/:id', (req, res) => {         //renders the main page
+  const {id} = req.params;                //returns string
+  let title = projects[parseInt(id)].project_name;
+  let description = projects[id].descritpion;
+  // let images =
+  let projectData = {title, description}
+  console.log(projectData);
+  res.render('project', {projectData});
+});
+
+module.exports = router;
