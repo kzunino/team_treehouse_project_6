1620d4bc093458398ef1bb4b865c7a6b740f50eb
Kyle <kzunino@gmail.com>
2019-07-04 12:19:06 -0700

    error handler functioning

--- routes/projects.js
+++ routes/projects.js
@@ -3,16 +3,22 @@ const router = express.Router();
 const {data} = require('../data/data.json'); //{data} referes to data.data
 const {projects} = data;      //data.projects es6 syntax.
 
-router.get('/:id', (req, res) => {         //renders the main page
-  const {id} = req.params;                //returns string
-  let title = projects[id].project_name;
-  let description = projects[id].descritpion;
-  let images = projects[id].image_urls;
-  let technologies = projects[id].technologies;
-  let liveLink = projects[id].live_link;
-  let gitHubLink = projects[id].github_link;
-  let projectData = {title, description, images, technologies, liveLink, gitHubLink};
-  res.render('project', {projectData});
+router.get('/:id', (req, res, next) => {         //renders the main page
+  const {id} = req.params;                      //returns string
+  if (projects[id]){
+    let title = projects[id].project_name;
+    let description = projects[id].descritpion;
+    let images = projects[id].image_urls;
+    let technologies = projects[id].technologies;
+    let liveLink = projects[id].live_link;
+    let gitHubLink = projects[id].github_link;
+    let projectData = {title, description, images, technologies, liveLink, gitHubLink};
+    res.render('project', {projectData});
+  }else{
+    const err = new Error('Project does not exist');
+    err.status = 404;
+    next(err);
+}
 });
 
 module.exports = router;
